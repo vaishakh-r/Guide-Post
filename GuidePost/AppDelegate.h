4e22f9535ad05260e8918cd0fafa23a5a3f8d432
Vaishakh T <vaishakh@Vaishakhs-MacBook-Pro.local>
2014-10-31 21:13:45 +0530

    Initial Commit

---
+++ GuidePost/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GuidePost
+//
+//  Created by Vaishakh on 10/31/14.
+//  Copyright (c) 2014 Eaagle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GuidePost/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GuidePost
+//
+//  Created by Vaishakh on 10/31/14.
+//  Copyright (c) 2014 Eaagle. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
